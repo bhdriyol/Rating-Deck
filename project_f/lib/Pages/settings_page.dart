e398d6945158f3c3c62202a46bbe3d9ddeae27a7
Bahadır İyol <bahadiriyol12@gmail.com>
2024-06-01 20:15:05 +0300

    Page Name Changes

--- project_f/lib/Pages/settings_page.dart
+++ project_f/lib/Pages/settings_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class UserPage extends StatelessWidget {
-  const UserPage({super.key});
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("User Page"),
+      child: Text("Settings Page"),
     );
   }
 }
